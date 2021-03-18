0f99e4d975d7d7ed731d7d43b6261fe92eb5755a
Rizaalft <Rizaalft@students.amikom.ac.id>
2021-03-18 08:13:38 +0700

    Program Taxi C#

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgramTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat Objek Taxi
+            Taxi03 taxi = new Taxi03();
+
+            // Pengesetan nilai propertis
+            taxi.Namadriver = " Rizaalft ";
+            taxi.Berkerja = true;
+            taxi.Penumpang = 10;
+
+            // Pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUp();
+            taxi.DropOff();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi03.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramTaxi
+{
+    class Taxi03
+    {
+        public string Namadriver { get; set; }
+        public bool Berkerja { get; set; }
+        public int Penumpang { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine(" Nama Pengemudi      : {0} ", Namadriver);
+            if (Berkerja == true)
+            {
+                Console.WriteLine(" Mengantar Penumpang : Ya ", Berkerja);
+            }
+            else
+            {
+                Console.WriteLine(" Tidak Mengantar Penumpang : Tidak ", Berkerja);
+            }
+
+            Console.WriteLine(" Jumlah Penumpang    : {0}\n ", Penumpang);
+        }
+
+        public void PickUp()
+        {
+            Console.WriteLine(" {0} Sedang Menjemput Penumpang ", Namadriver);
+        }
+
+        public void DropOff()
+        {
+            Console.WriteLine(" {0} Selesai Mengantar Penumpang\n ", Namadriver);
+        }
+    }
+}
